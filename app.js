9981247a5e40bf5a21963af633ed8049c4b63c44
Clam119 <Christopher.lam119@gmail.com>
2022-04-11 23:42:40 +0100

    Added GET route to compose.ejs

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
   res.render('home', {
     homeEjsContent:homeStartingContent
   });
@@ -32,6 +32,10 @@ app.get('/contact', function(req, res) {
   res.render('contact', {
     contactEjsContent:contactContent
   });
+});
+
+app.get('/compose', function(req, res) {
+  res.render('compose');
 });
 
 app.listen(process.env.PORT || port, function () {
