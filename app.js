2f8ff0313c3c2859b77476c3e15c5a8e076508b7
Clam119 <Christopher.lam119@gmail.com>
2022-04-12 22:32:58 +0100

    added posts array and rendering it to home.ejs

--- app.js
+++ app.js
@@ -2,47 +2,57 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const port = 3000;
+const posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('home', {
-    homeEjsContent:homeStartingContent
+    homeEjsContent: homeStartingContent,
+    posts:posts
   });
 });
 
 
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
   res.render('about', {
-    aboutEjsContent:aboutContent
+    aboutEjsContent: aboutContent
   });
 });
 
 
-app.get('/contact', function(req, res) {
+app.get('/contact', function (req, res) {
   res.render('contact', {
-    contactEjsContent:contactContent
+    contactEjsContent: contactContent
   });
 });
 
-app.get('/compose', function(req, res) {
-  res.render('compose');
+app.get('/compose', function (req, res) {
+  res.render('compose', {
+    newPostEjs:posts
+  });
 });
 
-app.post('/compose', function(req, res) {
-  const titlePost = req.body.titleComposePost;
-  const textPost = req.body.textComposePost;
-  console.log("Title: " + titlePost + "Post: " + textPost);
-})
+app.post('/compose', function (req, res) {
+  const post = {
+    title: req.body.titleComposePost,
+    text: req.body.textComposePost
+  };
+  posts.push(post);
+  res.redirect('/');
+});
 
 app.listen(process.env.PORT || port, function () {
   console.log("Server started on port 3000");
