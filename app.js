85e8b3b25947d7cba1f8c556c4b0c3c13c4d2485
Clam119 <Christopher.lam119@gmail.com>
2022-04-14 20:06:45 +0100

    Added rendering to post template ejs that contains post title & text

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(express.static("public"));
 
 app.get('/', function (req, res) {
@@ -31,12 +32,16 @@ app.get('/post/:postName', function (req, res) {
   let requestedTitle = _.lowerCase(req.params.postName);
 
   posts.forEach(function (post) {
-    let storedTitle = _.lowerCase(post.title);
 
-    if (requestedTitle == storedTitle) {
-      console.log("Match Found")
-    } else {
-      console.log("Match Not Found");
+    let storedTitle = _.lowerCase(post.title);
+    let postTitle = post.title;
+    let postText = post.text;
+
+    if (storedTitle === requestedTitle) {
+      res.render('post', {
+        postTitle: postTitle,
+        postText: postText,
+      });
     }
   })
 
